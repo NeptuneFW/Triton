87461f72a3ab8bddc1285e0269ee8b8a93e04341
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-14 01:13:31 +0300

    Development

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peker
+ * Date: 14.05.2017
+ * Time: 01:10
+ */
+
+foreach (glob('./*') as  $value)
+{
+    if($value !== 'index.php')
+    {
+        require_once $value;
+    }
+}
+
+$user = new UserModel();
+$user->name = "Emirhan";
+echo $user->name;
