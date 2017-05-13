276d847f07a69a7d30ade245ffcc0dd438a7e04a
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-14 01:09:11 +0300

    Development

--- Triton.php
+++ Triton.php
@@ -1,19 +1,20 @@
 <?php
+
+/**
+ * Created by PhpStorm.
+ * User: Peker
+ * Date: 14.05.2017
+ * Time: 01:03
+ */
 class Triton
 {
 
-<<<<<<< HEAD
     public function __get($name)
     {
-        // TODO: Implement __get() method.
     }
 
     public function __set($name, $value)
     {
-        // TODO: Implement __set() method.
     }
 
 }
-=======
-}
->>>>>>> a403263b64f47a10f42e59dafded91fcfa735a4b
