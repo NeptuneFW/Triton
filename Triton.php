7f995e605dd0fdbe4bfbb94fa7c51054a8baa0c3
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-14 01:06:44 +0300

    Development

--- Triton.php
+++ Triton.php
@@ -9,4 +9,14 @@
 class Triton
 {
 
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    public function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+    }
+
 }
