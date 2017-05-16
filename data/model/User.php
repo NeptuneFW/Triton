b940540b0466bef1d2d6c905f2dd6b15966e0229
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-16 20:58:21 +0300

    Development

--- data/model/User.php
+++ data/model/User.php
@@ -7,12 +7,12 @@ class User extends \Triton
     protected static
         $db = 'ntuser',
         $table = 'user';
-
+    /*
     public function forBuild()
     {
-        $this->name->oninsert = ucwords($string->name);
-        $this->name->onupdate = ucwords($string->name);
+        $this->name->oninsert = function($string) { ucwords($string->name); };
+        $this->name->onupdate = function($string) { ucwords($string->name); };
         $this->name->ondelete = function () { };
-
     }
+    */
 }
